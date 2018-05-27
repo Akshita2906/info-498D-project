01d568fd40ca8aa0b97d83d809a36907861aa6b5
Madhura Narkar <msn25@uw.edu>
2018-05-27 15:20:22 -0700

    Adding code for Healthcare knowledge

--- Prep.R
+++ Prep.R
@@ -2,18 +2,22 @@ library(Hmisc)
 library(foreign)
 library(survey)
 library(dplyr)
+library(tidyr)
+
 
 #Loading the data
-#BRFSS_all_data <- sasxport.get("data/LLCP2016XPT/LLCP2016.xpt")
-BRFSS_all_data <- sasxport.get("./data/LLCP2016.XPT")
+BRFSS_all_data <- sasxport.get("data/LLCP2016XPT/LLCP2016.xpt")
+#BRFSS_all_data <- sasxport.get("./data/LLCP2016.XPT")
 View(BRFSS_all_data)
 
-#--------------------------------------Health Care Access -------------------------------------------------------
+#--------------------------------------Health Care Access/knowledge -------------------------------------------------------
 
+unique(HCA_data$undrstnd)
 #Picking up columns of choice
-vars <- c("x.rfhlth", "hlthpln1", "persdoc2", "delaymed","medcost", "x.state", "x.llcpwt")
+vars <- c("x.rfhlth", "hlthpln1", "persdoc2", "delaymed","medcost", "x.state", "x.llcpwt", "undrstnd")
 HCA_data <- BRFSS_all_data[vars]
 #unique(HCA_data$x.rfhlth)
+
 
 #Converting the values into yes and no. Setting defaults for x.rfhlth column
 HCA_data$x.rfhlth <- replace(HCA_data$x.rfhlth, HCA_data$x.rfhlth==2,0)
@@ -80,6 +84,27 @@ svey_mean <- svymean(~medcost, svey, na.rm=TRUE)
 df_results[nrow(df_results) + 1,] = c("Adults with no cost constraints", 100 - svey_mean[1]*100)
 Z <- svyby(~medcost, ~x.state, svey, svymean, na.rm=TRUE)
 df_states<-data.frame("Adults with delay in healthcare due to cost",Z[1], Z[2]*100, stringsAsFactors = FALSE)
+names(df_states)<-c("Metric","State", "Value")
+
+
+
+#Converting the values into yes and no. Setting defaults for undrstnd column
+HCA_data<- HCA_data[na.omit(HCA_data$undrstnd), ]
+HCA_data$undrstnd <- replace(HCA_data$undrstnd, HCA_data$undrstnd==3,0)
+HCA_data$undrstnd <- replace(HCA_data$undrstnd, HCA_data$undrstnd==4,0)
+HCA_data$undrstnd <- replace(HCA_data$undrstnd, HCA_data$undrstnd==2,1)
+HCA_data$undrstnd <- replace(HCA_data$undrstnd, HCA_data$undrstnd==7,NA)
+HCA_data$undrstnd <- replace(HCA_data$undrstnd, HCA_data$undrstnd==9,NA)
+
+#Survey weights for undrstnd column 
+svey <- svydesign(ids=~1 ,strata=HCA_data$undrstnd, weights=HCA_data$undrstnd, nest=T, data=HCA_data)
+sveymean <- svymean(~undrstnd, svey, na.rm=TRUE)
+df_results<-data.frame("Adults who understand the language of medical professionals ",sveymean[1]*100, stringsAsFactors = FALSE)
+names(df_results)<-c("Metric","Value")
+Z <- svyby(~x.rfhlth, ~x.state, svey, svymean, na.rm=TRUE)
+
+#Storing the results into a dataframe for further use
+df_states<-data.frame("Adults with good health",Z[1], Z[2]*100, stringsAsFactors = FALSE)
 names(df_states)<-c("Metric","State", "Value")
 
 #--------------------------------Heart Disease------------------------------------------------------------------------
